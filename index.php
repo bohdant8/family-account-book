2575d0d27dd58e740681b86fbf79090ce728c897
chenslinc <chenweihua@sl-inc.co.jp>
2025-12-05 00:43:09 +0900

    put Chart.js 4.4.0  in local

--- index.php
+++ index.php
@@ -322,7 +322,7 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+    <script src="assets/libs/chart.js/chart.umd.min.js"></script>
     <script src="assets/js/app.js"></script>
 </body>
 </html>
